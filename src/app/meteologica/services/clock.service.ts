32c666235890afdccca55997e4868dfbac4762e7
Delbosque <rodrigo.m.delbosque@gmail.com>
2025-10-24 18:11:29 +0200

    Added comments to clock.service

--- src/app/meteologica/services/clock.service.ts
+++ src/app/meteologica/services/clock.service.ts
@@ -10,17 +10,23 @@ export class ClockService {
   }
 
   private startClock() {
-    if (this.clockTimeoutId) clearTimeout(this.clockTimeoutId); // Clears any previously saved weather data
+    // Clears any previously saved weather data
+    if (this.clockTimeoutId) clearTimeout(this.clockTimeoutId);
 
     const tick = () => {
-      const now = new Date(); // Get the current time
+      // Get the current time
+      const now = new Date();
       const hh = String(now.getHours()).padStart(2, '0');
       const mm = String(now.getMinutes()).padStart(2, '0');
       const ss = String(now.getSeconds()).padStart(2, '0');
+
+      // Updates the reactive signal. Automatically refreshes components.
       this.currentTime.set(`${hh}:${mm}:${ss}`);
 
-      // Precise adjustment to the next second
+      // Calculate how long until the next exact second
       const delay = 1000 - now.getMilliseconds();
+
+      // Schedule the next tick while maintaining precision
       this.clockTimeoutId = window.setTimeout(tick, delay);
     };
 
